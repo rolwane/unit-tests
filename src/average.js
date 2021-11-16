469be00c727ea79fecad4698acb8ae27a5df1275
Rolwane Borges <rolwane@hotmail.com>
2021-11-16 15:48:14 -0300

    Requisito 01

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) { return undefined; }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') { return undefined; }
+  }
+
+  let media = 0;
+
+  array.forEach((element) => {
+    media += element;
+  });
+
+  return Math.round(media / array.length);
+};
 
 module.exports = average;
