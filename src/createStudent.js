b6682bbe2c27d20ae81f21651325af66f157abe6
Rolwane Borges <rolwane@hotmail.com>
2021-11-16 16:59:05 -0300

    Requisito 05

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = function (nome) {
+  return {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+};
 
 module.exports = createStudent;
